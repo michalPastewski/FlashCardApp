e9f3704ae3fb723342c28a8a5ec069131a1dbb9a
MPastewski <michpastewski@gmail.com>
2023-06-28 10:41:13 +0200

    add top bar UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
-import './App.css'
+import './App.css';
+import {TopBar} from './components/topBar/TopBar'
 
 function App() {
 
   return (
-    <>
-      <div><h1>FiszkaApp</h1></div>
-    </>
+    <main className='main__container'>
+      <TopBar />
+      {/* <section className='top__bar'>
+        <div className='top__bar--logo'>FiszkaApp</div>
+        <div className='top__bar--search'>Search</div>
+      </section> */}
+      <section className='content'>
+        <article className='content__menu'>
+          <div>add new word</div>
+          <div>learn</div>
+        </article>
+        <article className='content__words'>
+          <div className='content__words'>Words List</div>
+          <div className='content__words--container'>
+            <div className='content__words--item'>word 1</div>
+            <div className='content__words--item'>word 2</div>
+            <div className='content__words--item'>word 3</div>
+          </div>
+        </article>
+      </section>
+    </main>
   )
 }
 
