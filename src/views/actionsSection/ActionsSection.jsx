471d1e1c61c0de350b6906742a1331a12ded65e5
MPastewski <michpastewski@gmail.com>
2023-07-07 10:54:32 +0200

    improve form input UI

--- src/views/actionsSection/ActionsSection.jsx
+++ src/views/actionsSection/ActionsSection.jsx
@@ -1,30 +1,43 @@
 import { useState } from 'react';
 import { Button } from '../../components/Button';
-import { FormInput } from '../../components/FormInput';
+import { AddWordForm } from './AddWorldForm';
 import './actionsSection.style.scss';
 
 export const ActionsSection = () => {
-   const [addForm, setAddForm]= useState(false);
+  const [addForm, setAddForm] = useState(false);
 
-   return (
-      <article className='actions'>
-         <section className='actions__buttons'>
-            <Button label='add new world' size='large' onClick={()=> {setAddForm(true)}}/>
-            <Button label='learn' size='large' onClick={()=>console.log('learn')}/>
-         </section>
-
-      {addForm && (
-         <form className='add__form'>
-            <FormInput label="new word" type='text' />             
-            <FormInput label="translation" type='text' />             
-            <FormInput label="synonyms" type='text' />             
-            <FormInput label="examples" type='text' />    
-            <div className='add__form__buttons'>
-               <Button label="cancel" type='button' onClick={() => {setAddForm(false)}}/>
-               <Button label="add" buttonstyle='submit' />
-            </div>         
-         </form>
-      )}
-      </article>
-   )
-}
+  return (
+    <article className="actions">
+      <section className="actions__buttons">
+        {addForm ? (
+          <>
+            <Button
+              label="add new world"
+              size="large"
+              onClick={() => {
+                setAddForm(true);
+              }}
+            />
+            <Button label="learn" size="large" disabled />
+          </>
+        ) : (
+          <>
+            <Button
+              label="add new world"
+              size="large"
+              onClick={() => {
+                setAddForm(true);
+              }}
+            />
+            <Button
+              label="learn"
+              size="large"
+              onClick={() => console.log('learn')}
+            />
+          </>
+        )}
+      </section>
+      {addForm && <AddWordForm onCancel={setAddForm} />}
+    </article>
+  );
+};
