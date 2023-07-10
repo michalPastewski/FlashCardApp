f30d8602a270b0a2a38effe07aaae2f209c51a85
MPastewski <michpastewski@gmail.com>
2023-07-10 13:48:39 +0200

    add close/open learn section handler

---
+++ src/views/actionsSection/LearnSection.jsx
@@ -0,0 +1,11 @@
+import { Button } from '../../components/Button';
+
+export const LearnSection = () => {
+  return (
+    <div className="learn__section">
+      <Button label="learn" buttonstyle="secondary" />
+      <Button label="repeat" buttonstyle="secondary" />
+      <Button label="remind" buttonstyle="secondary" />
+    </div>
+  );
+};
